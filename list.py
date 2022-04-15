26746639960f87675ee7085f4f2dfedc5a1c1cb4
deogicorgi <deogicorgi@gmail.com>
2022-04-15 21:07:45 +0900

    리스트 공부
    리스트 공부

---
+++ list.py
@@ -0,0 +1,26 @@
+my_list = []
+my_list2 = list()
+my_list3 = [10, 20, 30]
+
+print(type(my_list))
+print(type(my_list2))
+print(type(my_list3))
+
+print(len(my_list3))
+
+my_list4 = [1, "2", 3.1, [1, 2, 3]]
+
+for i in my_list4:
+    print(type(i))
+
+
+print('>>>>>')
+
+print(my_list4[0])
+
+print(my_list4[0:4])
+print(my_list4[0:])
+print(my_list4[1:])
+
+str = "test"
+print(str)
